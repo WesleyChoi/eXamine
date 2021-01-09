8dab27efb3b6e20369a79400f0ca4955c6048b49
Wesley <wesleyhchoi@gmail.com>
2021-01-09 15:53:05 -0800

    Added image opening system

--- Basic.py
+++ Basic.py
@@ -2,3 +2,12 @@ import cv2
 import numpy
 import face_recognition
 
+imageSanta = face_recognition.load_image_file('imagesExamples/Santa.jpeg')
+imageSanta = cv2.cvtColor(imageSanta, cv2.COLOR_BGR2RGB)
+
+imageTestSanta = face_recognition.load_image_file('imagesExamples/Santa2.jpg')
+imageTestSanta = cv2.cvtColor(imageTestSanta, cv2.COLOR_BGR2RGB)
+
+cv2.imshow('Santa Ono',imageSanta)
+cv2.imshow('Santa Ono Test',imageTestSanta)
+cv2.waitKey(0)
