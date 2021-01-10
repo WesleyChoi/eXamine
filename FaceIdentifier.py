c86c50295c4d5529ce9e6a87bc4e1019295c6d56
Raymond Liu <raymliu@hotmail.com>
2021-01-09 21:40:00 -0800

    Refactored FaceIdentifier

--- FaceIdentifier.py
+++ FaceIdentifier.py
@@ -2,7 +2,55 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from RecognizeFaces import SIDRecognize, ZoomRecognize
 
+class FaceIdentifier(object):
+    def __init__(self, studentImagesPath, screenshotImagesPath):
+        self.studentImagesPath = studentImagesPath
+        self.screenshotImagesPath = screenshotImagesPath
+        self.images = []
+        self.classNames = []
+        self.presentStudents = os.listdir(self.studentImagesPath)
+
+        self.retrieve_names()
+        #self.upload_screenshots()
+
+    def retrieve_names(self):
+        for student in self.presentStudents:
+            currentImage = cv2.imread(f'{self.studentImagesPath}/{student}')
+            self.images.append(currentImage)
+            self.classNames.append(os.path.splitext(student)[0])
+        print(self.classNames)
+
+    # unfinished
+    def upload_screenshots(self):
+        # In the console, make sure the user includes the file type
+        # Also have some return in the UI if the user does not type an existing file
+        print('What is the name of your screenshot image file? Type \'Done\' if you are done.')
+        response = str(input())
+
+        while response != 'Done':
+            imageToConvert = self.screenshotImagesPath + '/' + response
+
+            try:
+                classImage = face_recognition.load_image_file(imageToConvert)
+            except IOError:
+                print('File location was not valid. ' +
+                      'Make sure that your file was typed correctly, including the file extension.')
+
+            print('What is the name of your screenshot image file? Type \'Done\' if you are done.')
+            response = str(input())
+
+    def recognize_faces(self):
+        students = SIDRecognize(self.studentImagesPath)
+        zoomScreenshots = ZoomRecognize(self.screenshotImagesPath)
+        students.encode()
+        zoomScreenshots.recognize(students)
+
+if __name__ == "__main__":
+    faceIdentifier = FaceIdentifier('ImagesAttendance', 'ZoomImages')
+    faceIdentifier.recognize_faces()
+"""
 # retrieve images from ImagesAttendance folder
 path = 'ImagesAttendance'
 images = []
@@ -83,3 +131,4 @@ while True:
 # results = face_recognition.compare_faces([encodeSanta],encodeSantaTest)
 #
 # faceDistance = face_recognition.face_distance([encodeSanta],encodeSantaTest)
+"""
